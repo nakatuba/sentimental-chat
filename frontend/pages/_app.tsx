622a4b0ed4e61a70403c09dcf6f0f80a786baaa9
nakatuba <nakatuba0626@gmail.com>
2022-09-06 11:37:55 +0900

    Install next-auth

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,11 +1,14 @@
 import '../styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
     </ChakraProvider>
   )
 }
---
+++ frontend/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,70 @@
+import NextAuth from 'next-auth'
+import type { JWT } from 'next-auth/jwt'
+import CredentialsProvider from 'next-auth/providers/credentials'
+
+async function verifyAccessToken(token: JWT) {
+  const res = await fetch('http://backend:8000/api/auth/jwt/verify/', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ refresh: token.accessToken }),
+  })
+
+  return res.ok
+}
+
+async function refreshAccessToken(token: JWT) {
+  const res = await fetch('http://backend:8000/api/auth/jwt/refresh/', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ refresh: token.refreshToken }),
+  })
+  const { access } = await res.json()
+
+  return {
+    accessToken: access,
+    refreshToken: token.refreshToken,
+  }
+}
+
+export default NextAuth({
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        username: { type: 'text' },
+        password: { type: 'password' },
+      },
+      async authorize(credentials) {
+        const res = await fetch('http://backend:8000/api/auth/jwt/create/', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(credentials),
+        })
+        const user = await res.json()
+
+        if (res.ok && user) {
+          return user
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        return {
+          accessToken: user.access,
+          refreshToken: user.refresh,
+        }
+      }
+
+      if (await verifyAccessToken(token)) {
+        return token
+      }
+
+      return refreshAccessToken(token)
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken
+      return session
+    },
+  },
+})
