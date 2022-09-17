ccce1cb7819174069a05506bf2d6fbd2dff3b828
nakatuba <nakatuba0626@gmail.com>
2022-09-17 16:46:23 +0900

    Add sentiment score to message model

--- backend/api/models.py
+++ backend/api/models.py
@@ -8,7 +8,21 @@ class User(AbstractUser, UUIDModel):
     REQUIRED_FIELDS = ['icon']
 
 
+class SentimentScore(UUIDModel):
+    joy = models.FloatField()
+    sadness = models.FloatField()
+    anticipation = models.FloatField()
+    surprise = models.FloatField()
+    anger = models.FloatField()
+    fear = models.FloatField()
+    disgust = models.FloatField()
+    trust = models.FloatField()
+
+
 class Message(UUIDModel):
     created_at = models.DateTimeField(auto_now_add=True)
     sender = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
+    sentiment_score = models.OneToOneField(
+        SentimentScore, on_delete=models.CASCADE, null=True, blank=True
+    )
