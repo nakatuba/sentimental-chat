f089defde4f0142b10ebd5d0dd5a2b27fb4d81de
nakatuba <nakatuba0626@gmail.com>
2022-09-17 15:59:15 +0900

    Add message date

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import moment from 'moment'
+import 'moment-timezone'
 import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
+
+moment.tz.setDefault('Asia/Tokyo')
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
