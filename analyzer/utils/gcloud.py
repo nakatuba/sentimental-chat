a8cc989c4a556d25fb48877004d6282524d67ff3
nakatuba <nakatuba0626@gmail.com>
2023-01-08 16:57:51 +0900

    Fix analyzer

---
+++ analyzer/utils/gcloud.py
@@ -0,0 +1,15 @@
+import io
+from typing import Any
+
+import cloudpickle
+from google.cloud import storage
+
+
+def download_from_gcs(bucket_name: str, blob_name: str) -> Any:
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    contents = blob.download_as_string()
+
+    with io.BytesIO(contents) as f:
+        return cloudpickle.load(f)
