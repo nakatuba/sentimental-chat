dcce57d33592f8f84dea052550e2d04fbf8b6579
nakatuba <nakatuba0626@gmail.com>
2023-01-14 19:58:07 +0900

    Fix early stopping

--- analyzer/utils/early_stopping.py
+++ analyzer/utils/early_stopping.py
@@ -3,8 +3,9 @@ from typing import Literal
 
 class EarlyStopping:
     def __init__(self, patience: int, mode: Literal["min", "max"] = "min") -> None:
-        self.best = None
         self.patience = patience
+        self.best = None
+        self.num_bad_epochs = 0
         if mode == "min":
             self.is_better = lambda a, best: a < best
         elif mode == "max":
