27aa4f8c20ef7bfda7ea402b414d981fb5b4b3f5
nakatuba <nakatuba0626@gmail.com>
2023-03-25 12:53:34 +0900

    Remove validation

--- analyzer/train.py
+++ analyzer/train.py
@@ -5,7 +5,6 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import wandb
-from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader
 from transformers import AutoTokenizer
 
@@ -14,7 +13,6 @@ from model import WrimeBert
 from utils.args import get_args
 from utils.collator import WrimeCollator
 from utils.dataset import WrimeDataset
-from utils.early_stopping import EarlyStopping
 from utils.seed import fix_seed
 
 
@@ -40,19 +38,6 @@ def train(
     return epoch_loss / len(dataloader)
 
 
-def evaluate(model: nn.Module, dataloader: DataLoader, criterion: nn.Module) -> float:
-    model.eval()
-    epoch_loss = 0
-
-    with torch.no_grad():
-        for input, label in dataloader:
-            output = model(input)
-            loss = criterion(output, label)
-            epoch_loss += loss.item()
-
-    return epoch_loss / len(dataloader)
-
-
 def main() -> None:
     args = get_args()
 
@@ -64,12 +49,7 @@ def main() -> None:
 
     df = pd.read_csv(args.wrime_tsv, sep="\t")
 
-    train_df, valid_df = train_test_split(
-        df, test_size=args.validation_size, random_state=args.seed
-    )
-
-    train_dataset = WrimeDataset(train_df, emotions=args.emotions)
-    valid_dataset = WrimeDataset(valid_df, emotions=args.emotions)
+    train_dataset = WrimeDataset(df, emotions=args.emotions)
 
     tokenizer = AutoTokenizer.from_pretrained(args.pretrained_model)
     collator = WrimeCollator(tokenizer, device=device)
@@ -80,12 +60,6 @@ def main() -> None:
         shuffle=True,
         collate_fn=collator,
     )
-    valid_dataloader = DataLoader(
-        valid_dataset,
-        batch_size=args.batch_size,
-        shuffle=False,
-        collate_fn=collator,
-    )
 
     model = WrimeBert(
         pretrained_model=args.pretrained_model,
@@ -94,19 +68,15 @@ def main() -> None:
     ).to(device)
     criterion = nn.MSELoss().to(device)
     optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
-    early_stopping = EarlyStopping(patience=args.patience)
 
     for epoch in range(args.num_epochs):
         train_loss = train(model, train_dataloader, criterion, optimizer)
-        valid_loss = evaluate(model, valid_dataloader, criterion)
-        print(
-            f"Epoch {epoch + 1}/{args.num_epochs} | train | Loss: {train_loss:.4f} | valid | Loss: {valid_loss:.4f}"
-        )
-        if early_stopping(valid_loss, model):
-            break
+        print(f"Epoch {epoch + 1}/{args.num_epochs} | train | Loss: {train_loss:.4f}")
 
-    analyzer = WrimeAnalyzer(early_stopping.best_model, tokenizer, args.emotions)
-    analyzer.save(os.path.join(wandb.run.dir, "analyzer.pkl"))
+    analyzer = WrimeAnalyzer(model, tokenizer, args.emotions)
+
+    if wandb.run:
+        analyzer.save(os.path.join(wandb.run.dir, "analyzer.pkl"))
 
 
 if __name__ == "__main__":
