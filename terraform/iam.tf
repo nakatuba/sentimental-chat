41e30e1fd58451cfeb1165b03c35d72051bd7b95
nakatuba <nakatuba0626@gmail.com>
2023-05-28 23:38:02 +0900

    Add roles to access secret manager

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,15 +1,19 @@
-locals {
-  roles = [
-    "roles/iam.serviceAccountUser",
-    "roles/run.admin",
-  ]
-}
-
 data "google_project" "project" {
 }
 
-resource "google_project_iam_member" "iam" {
-  for_each = toset(local.roles)
+resource "google_project_iam_member" "default_compute_sa_iam" {
+  project = var.project_id
+  role    = "roles/secretmanager.secretAccessor"
+  member  = "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
+}
+
+resource "google_project_iam_member" "cloudbuild_sa_iam" {
+  for_each = toset([
+    "roles/cloudsql.client",
+    "roles/iam.serviceAccountUser",
+    "roles/run.admin",
+    "roles/secretmanager.secretAccessor"
+  ])
 
   project = var.project_id
   role    = each.value
