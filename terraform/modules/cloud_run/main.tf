9726fb4f4881771fd54eabd8ca7046932723a841
nakatuba <nakatuba0626@gmail.com>
2023-05-30 06:43:45 +0900

    Add ignore changes to cloud run service

--- terraform/modules/cloud_run/main.tf
+++ terraform/modules/cloud_run/main.tf
@@ -18,4 +18,11 @@ resource "google_cloud_run_service" "service" {
       annotations = var.template_annotations
     }
   }
+
+  lifecycle {
+    ignore_changes = [
+      template[0].metadata[0].annotations["run.googleapis.com/client-name"],
+      template[0].metadata[0].annotations["run.googleapis.com/client-version"]
+    ]
+  }
 }
