750ee998627bc99c129e0d00297d8fc0cb20aa38
nakatuba <nakatuba0626@gmail.com>
2023-06-09 22:58:53 +0900

    Add room page

--- frontend/pages/rooms/[id].tsx
+++ frontend/pages/rooms/[id].tsx
@@ -1,6 +1,6 @@
-import Header from '../components/header'
-import MessageBox from '../components/message-box'
-import type { Message, User } from '../interfaces'
+import { RoomHeader } from 'components/header'
+import { MessageBox } from 'components/message-box'
+import type { Room } from 'interfaces'
 import {
   Box,
   Center,
@@ -19,24 +19,23 @@ import { IoSend } from 'react-icons/io5'
 import TextareaAutosize from 'react-textarea-autosize'
 
 type Props = {
-  user: User
-  messages: Message[]
+  room: Room
 }
 
-export default function Home(props: Props) {
+export default function Room(props: Props) {
   const router = useRouter()
   const socketRef = useRef<WebSocket>()
   const bottomBoxRef = useRef<HTMLDivElement>(null)
   const sendButtonRef = useRef<HTMLButtonElement>(null)
   const { data: session } = useSession()
-  const [messages, setMessages] = useState(props.messages)
+  const [messages, setMessages] = useState(props.room.messages)
 
   useEffect(() => {
     socketRef.current = new WebSocket(
       `${process.env.NEXT_PUBLIC_BACKEND_HOST?.replace(
         /^http(s)?\:\/\//,
         'ws$1://'
-      )}/chat/`
+      )}/ws/chat/${props.room.id}/`
     )
 
     socketRef.current.onmessage = function (e) {
@@ -47,7 +46,7 @@ export default function Home(props: Props) {
     return () => {
       socketRef.current?.close()
     }
-  }, [])
+  }, [props.room.id])
 
   useEffect(() => bottomBoxRef.current?.scrollIntoView(), [messages])
 
@@ -65,12 +64,17 @@ export default function Home(props: Props) {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
+          room: props.room.id,
           body: target.body.value,
         }),
       }
     )
-    if (!res.ok && res.status === 401) {
-      router.push('/login')
+
+    if (!res.ok) {
+      if (res.status === 401) {
+        router.push('/login')
+      }
+      return
     }
 
     const message = await res.json()
@@ -80,80 +84,82 @@ export default function Home(props: Props) {
   }
 
   return (
-    <Flex flexDirection="column" bg="gray.100" minH="100vh">
-      <Header user={props.user} />
-      <Box flexGrow={1}>
-        {messages.map((message, index) => (
-          <Box key={message.id}>
-            {(index === 0 ||
-              moment(message.created_at).isAfter(
-                messages[index - 1].created_at,
-                'day'
-              )) && (
-              <Center>
-                <Box
-                  px={4}
-                  my={2}
-                  fontWeight="bold"
-                  border="1px"
-                  borderColor="gray"
-                  borderRadius="full"
-                  display="inline-block"
-                >
-                  {(() => {
-                    const messageDate = moment(message.created_at)
-                    const today = moment()
-                    const yesterday = moment().subtract(1, 'days')
+    <>
+      <RoomHeader room={props.room} />
+      <Flex flexDirection="column" pt={24} minH="100vh" bg="gray.100">
+        <Box flexGrow={1}>
+          {messages.map((message, index) => (
+            <Box key={message.id}>
+              {(index === 0 ||
+                moment(message.created_at).isAfter(
+                  messages[index - 1].created_at,
+                  'day'
+                )) && (
+                <Center>
+                  <Box
+                    px={4}
+                    my={2}
+                    fontWeight="bold"
+                    border="1px"
+                    borderColor="gray"
+                    borderRadius="full"
+                    display="inline-block"
+                  >
+                    {(() => {
+                      const messageDate = moment(message.created_at)
+                      const today = moment()
+                      const yesterday = moment().subtract(1, 'days')
 
-                    if (messageDate.isSame(today, 'day')) {
-                      return '今日'
-                    } else if (messageDate.isSame(yesterday, 'day')) {
-                      return '昨日'
-                    } else {
-                      return messageDate.format('YYYY年MM月DD日')
-                    }
-                  })()}
-                </Box>
-              </Center>
-            )}
-            <MessageBox message={message} />
-          </Box>
-        ))}
-        <Box ref={bottomBoxRef} />
-      </Box>
-      <HStack
-        as="form"
-        p={4}
-        spacing={4}
-        bg="white"
-        position="sticky"
-        bottom={0}
-        alignItems="end"
-        onSubmit={sendMessage}
-      >
-        <Textarea
-          name="body"
-          as={TextareaAutosize}
-          maxRows={8}
-          resize="none"
-          onKeyDown={e => {
-            if (e.key === 'Enter' && e.metaKey) {
-              e.preventDefault()
-              sendButtonRef.current?.click()
-            }
-          }}
-        />
-        <IconButton
-          aria-label="Send message"
-          icon={<IoSend />}
-          type="submit"
-          bg="blue.400"
-          color="white"
-          _hover={{ bg: 'blue.500' }}
-          ref={sendButtonRef}
-        ></IconButton>
-      </HStack>
-    </Flex>
+                      if (messageDate.isSame(today, 'day')) {
+                        return '今日'
+                      } else if (messageDate.isSame(yesterday, 'day')) {
+                        return '昨日'
+                      } else {
+                        return messageDate.format('YYYY年MM月DD日')
+                      }
+                    })()}
+                  </Box>
+                </Center>
+              )}
+              <MessageBox message={message} />
+            </Box>
+          ))}
+          <Box ref={bottomBoxRef} />
+        </Box>
+        <HStack
+          as="form"
+          p={4}
+          spacing={4}
+          bg="white"
+          position="sticky"
+          bottom={0}
+          alignItems="end"
+          onSubmit={sendMessage}
+        >
+          <Textarea
+            name="body"
+            as={TextareaAutosize}
+            maxRows={8}
+            resize="none"
+            onKeyDown={e => {
+              if (e.key === 'Enter' && e.metaKey) {
+                e.preventDefault()
+                sendButtonRef.current?.click()
+              }
+            }}
+          />
+          <IconButton
+            aria-label="Send message"
+            icon={<IoSend />}
+            type="submit"
+            bg="blue.400"
+            color="white"
+            _hover={{ bg: 'blue.500' }}
+            ref={sendButtonRef}
+          ></IconButton>
+        </HStack>
+      </Flex>
+    </>
   )
 }
 
@@ -161,34 +167,27 @@ export async function getServerSideProps(ctx: GetServerSidePropsContext) {
   const token = await getToken({ req: ctx.req })
 
   const res = await fetch(
-    `${process.env.BACKEND_HOST}/api/messages?ordering=created_at`,
+    `${process.env.BACKEND_HOST}/api/rooms/${ctx.query.id}`,
     {
       headers: {
         Authorization: `Bearer ${token?.accessToken}`,
       },
     }
   )
-  const messages = await res.json()
 
-  if (!res.ok && res.status === 401) {
+  if (!res.ok) {
     return {
       redirect: {
-        destination: '/login',
+        destination: '/',
         permanent: false,
       },
     }
   }
 
-  const user = await fetch(`${process.env.BACKEND_HOST}/api/users/me/`, {
-    headers: {
-      Authorization: `Bearer ${token?.accessToken}`,
-    },
-  }).then(res => res.json())
-
+  const room = await res.json()
   return {
     props: {
-      user,
-      messages,
+      room,
     },
   }
 }
