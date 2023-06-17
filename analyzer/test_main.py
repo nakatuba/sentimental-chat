ec0c48034d14eef1901b47d10ec09efded25f0fa
nakatuba <nakatuba0626@gmail.com>
2023-06-18 00:47:16 +0900

    Add analyzer test

---
+++ analyzer/test_main.py
@@ -0,0 +1,25 @@
+from faker import Faker
+from fastapi.testclient import TestClient
+
+from main import app
+
+client = TestClient(app)
+fake = Faker("ja_jp")
+
+
+def test_analyze():
+    response = client.post("/analyze", json={"body": fake.text()})
+    assert response.status_code == 200
+    for emotion in [
+        "joy",
+        "sadness",
+        "anticipation",
+        "surprise",
+        "anger",
+        "fear",
+        "disgust",
+        "trust",
+    ]:
+        score = response.json().get(emotion)
+        assert isinstance(score, float)
+        assert 0 <= score <= 1
