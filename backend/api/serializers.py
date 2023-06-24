ad7c64fe33ec9e3f5bb10f50790db367bdb4379d
nakatuba <nakatuba0626@gmail.com>
2023-06-24 21:37:34 +0900

    Remove reverse relationship

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -7,32 +7,17 @@ User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
-    rooms = serializers.SerializerMethodField()
-
     class Meta:
         model = User
         fields = '__all__'
 
-    def get_rooms(self, obj):
-        rooms = obj.rooms.order_by('created_at')
-        return rooms.values('id', 'created_at', 'name')
-
 
 class RoomSerializer(serializers.ModelSerializer):
     owner = UserSerializer(read_only=True)
-    messages = serializers.SerializerMethodField()
 
     class Meta:
         model = Room
         fields = '__all__'
-
-    def save(self, **kwargs):
-        owner = self.context['request'].user
-        return super().save(owner=owner, **kwargs)
-
-    def get_messages(self, obj):
-        messages = obj.messages.order_by('created_at')
-        return MessageSerializer(messages, many=True, context=self.context).data
 
 
 class SentimentScoreSerializer(serializers.ModelSerializer):
@@ -43,6 +28,7 @@ class SentimentScoreSerializer(serializers.ModelSerializer):
 
 class MessageSerializer(serializers.ModelSerializer):
     sender = UserSerializer(read_only=True)
+    room = RoomSerializer(read_only=True)
     sentiment_score = SentimentScoreSerializer(read_only=True)
 
     class Meta:
